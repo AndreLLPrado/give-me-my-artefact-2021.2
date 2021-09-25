056d29ce5e8edf5c1498e4c6bfde63eabb97de96
André Prado <andrele.prado@gmail.com>
2021-09-25 10:36:42 -0300

    timer

--- Give_Me_My_Artefact_2021.2/Assets/Scripts/MenuScript.cs
+++ Give_Me_My_Artefact_2021.2/Assets/Scripts/MenuScript.cs
@@ -6,16 +6,19 @@ using MLAPI;
 public class MenuScript : MonoBehaviour
 {
     public GameObject menuPanel;
+    public GameObject timerTextObj;
 
     public void Host()
     {
         NetworkManager.Singleton.StartHost();
         menuPanel.SetActive(false);
+        timerTextObj.SetActive(true);
     }
 
     public void Join()
     {
         NetworkManager.Singleton.StartClient();
         menuPanel.SetActive(false);
+        timerTextObj.SetActive(true);
     }
 }
---
+++ Give_Me_My_Artefact_2021.2/Assets/Scripts/Timer.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAPI;
+using MLAPI.NetworkVariable;
+
+public class Timer : MonoBehaviour
+{
+    public float timer; //time in seconds
+    public bool timeOver;
+    public bool startTimer;
+    public GameObject timerPanel;
+    void Start()
+    {
+        timeOver = false;
+        startTimer = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            startTimer = true;
+        }
+        if (!timeOver && startTimer)
+        {
+            runTimer();
+        }
+
+        //timerPanel.GetComponent<TextEditor>().text = timer.ToString();
+    }
+
+    public void runTimer()
+    {
+        timer -= 1f * Time.deltaTime;
+
+        if(timer <= 0f)
+        {
+            timeOver = true;
+        }
+    }
+}
